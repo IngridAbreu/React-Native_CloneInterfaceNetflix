034e98e1097c863be14671f5d5b15d6a24ea348b
IngridAbreu <softwares.abreu@gmail.com>
2020-10-01 19:58:27 -0300

    .

---
+++ Netflix/App.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+import Login from './src/screen/Login'
+
+import Home from './src/screen/Home'
+
+const App = () => {
+  return (
+    <>
+      <Login />
+      <Home />
+    </>
+  )
+}
+export default App;
